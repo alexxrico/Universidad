7f7b96b0f770a7dd072fc511ac10186a3888d5c2
alexxrico <alexxricoart@gmail.com>
2021-02-12 23:17:24 -0700

    Codigo de arbol binario de busqueda,inserta,elimina,busca, maximo/minimo

---
+++ ArbolBinario.c
@@ -0,0 +1,177 @@
+/*
+	...Estructuras de datos avanzadas...
+	Tema: Arboles binarios de busqueda
+	Autor: David Alejandro Perez Rico
+	Matricula: 320753
+    Fecha de creacion: 12/02/2021
+	....................................
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct snodo
+{
+    int valor;
+    struct snodo *izq, *der;
+}tnodo;
+
+tnodo *arbol=NULL, *aux2=NULL;
+
+int insertar(int x);
+tnodo * buscar(int x);
+
+tnodo *minimo(void);
+tnodo *maximo(void);
+
+int eliminar(int x);
+int altura(tnodo nodo);
+
+int main(int argc, const char * argv[]) {
+    int x, y,z,opc;
+    tnodo *xb;
+        do{
+            printf("\n1) Insertar \n2) Buscar \n3) Eliminar \n4) Minimo \n5) Maximo\n6) Altura \n7) Salir\nOpcion:");
+            scanf("%i",&opc);
+            switch(opc)
+            {
+                case 1: printf("\nValor  insertar: "); scanf("%i", &x);
+                        if(insertar(x)==1)
+                            printf("\nSi se pudo insertar");
+                        else
+                            printf("\nEl numero ya fue agregado");
+                        break;
+                case 2: printf("\nValor  buscar: "); scanf("%i", &x);
+                        xb=buscar(x);
+                        if(xb==NULL)
+                            printf("\nEl valor no existe");
+                        else
+                            printf("\nSI existe es %i", xb->valor);
+                    break;
+                case 3: printf("\nValor  eliminar: "); scanf("%i", &x);
+                		z = eliminar(x);
+                		if(z==1){
+                			printf("\nEl numero fue eliminado con exito!");
+						}else{
+							printf("\nEl numero no existe...");
+						}
+                    break;
+                case 4:
+                    xb=minimo();
+                    if(xb!=NULL)
+                        printf("\nEl minimo es %i", xb->valor);
+                    else
+                        printf("\nNo hay minimo, esta vacio");
+                    break;
+                case 5: xb=maximo();
+                    if(xb!=NULL)
+                        printf("\nEl maximo es %i", xb->valor);
+                    else
+                        printf("\nNo hay maximo, esta vacio");
+                    break;
+                case 6:{
+
+                  break;
+                }
+
+            }
+        }while(opc!=7);
+
+}
+int altura(tnodo nodo){
+    int x;
+
+    return x;
+}
+int insertar(int x)
+{
+    tnodo *n1;
+    int pude=0;
+    n1 = buscar(x);
+    if(n1==NULL)
+    {
+        n1 = (tnodo *)malloc(sizeof(tnodo));
+        if(n1!=NULL)
+        {
+            n1->valor = x;
+            n1->der = NULL;
+            n1->izq=NULL;
+            if(arbol==NULL)
+                arbol=n1;
+            else if(x < aux2->valor  )
+                aux2->izq = n1;
+            else
+                aux2->der = n1;
+            pude = 1;
+        }
+    }
+    return pude;
+}
+
+tnodo * buscar(int x)
+{
+    tnodo *aux;
+    aux =arbol;
+    aux2=NULL;
+    while(aux!=NULL)
+    {
+        aux2=aux;
+        if(aux->valor == x)
+            break;
+        else if(x < aux->valor)
+            aux = aux->izq;
+        else
+            aux = aux->der;
+    }
+    return aux;
+}
+tnodo * minimo(void)
+{
+    tnodo *aux;
+    aux =arbol;
+    aux2=NULL;
+    while(aux!=NULL)
+    {
+        aux2= aux;
+        aux = aux->izq;
+    }
+    return aux2;
+}
+tnodo * maximo(void)
+{
+    tnodo *aux;
+    aux =arbol;
+    aux2=NULL;
+    while(aux!=NULL)
+    {
+        aux2= aux;
+        aux = aux->der;
+    }
+    return aux2;
+}
+int eliminar(int x){
+	tnodo *nElim, *ant, *aux;
+	nElim=buscar(x);
+	if(nElim==NULL){
+		return 0;
+	}else{
+		aux=nElim;
+		if(aux==arbol){
+			free(aux);
+			arbol==NULL;
+		}else{
+			aux=aux->izq;
+			if(aux!=NULL){
+				while(aux->der!=NULL){
+					ant=aux;
+					aux=aux->der;
+				}
+				nElim->valor=ant->valor;
+				ant->der=NULL;
+				free(ant);
+			}else{
+				free(nElim);
+			}
+		}
+		return 1;
+	}
+}
